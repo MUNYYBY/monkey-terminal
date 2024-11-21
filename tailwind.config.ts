35a166a56dbaf329177d05d00ef1b792be6bf716
MUNYYBY <hm.muneeb.u.r@gmail.com>
2024-11-21 15:58:48 +0500

    feat: completed landing page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,33 @@
 import type { Config } from "tailwindcss";
 
-export default {
+const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: "class",
   theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1rem",
+        sm: "2rem",
+        lg: "4rem",
+        xl: "5rem",
+        "2xl": "6rem",
+      },
+    },
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: "#26b83d",
+      },
+      spacing: {
+        88: "22rem",
       },
     },
   },
   plugins: [],
-} satisfies Config;
+};
+export default config;
